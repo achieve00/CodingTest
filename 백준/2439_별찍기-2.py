8103bc1a4b133e1925459ea6c971b531c8ef24ff
achieve00 <achieve0v0@gmail.com>
2025-05-20 12:50:26 +0900

    2025-05-20

---
+++ 백준/2439_별찍기-2.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for i in range(1, n+1):
+  j = n - i
+  print(' ' * j, end='')
+  print('*' * i)
---
+++ 백준/2739_구구단.py
@@ -0,0 +1,4 @@
+n = int(input())
+
+for G in range(1,10): 
+    print(f"{n} * {G} = {n*G}")
